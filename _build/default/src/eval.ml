74bb48277d12c2f9af6cde3c77db51cda3396f44
EnricJiao <enricjiao3@gmail.com>
2023-09-24 20:25:44 -0400

    Initial commit

--- _build/default/src/eval.ml
+++ _build/default/src/eval.ml
@@ -11,7 +11,7 @@ exception DivByZeroError
   You do not have to use these and you can modify them if you want. 
   If you do not use references, you will need the following data type:
 *)
-type values = Int of int|Bool of bool|String of string
+(*type values = Int of int|Bool of bool|String of string*)
 
 (* Adds mapping [x:v] to environment [env] *)
 let ref_extend env x v = (x, ref v)::env
@@ -23,7 +23,7 @@ let extend env x v = (x,v)::env
 let rec ref_lookup env x =
   match env with
   | [] -> raise (DeclareError ("Unbound variable " ^ x))
-  | (var, value)::t -> if x = var then !value else ref-lookup t x
+  | (var, value)::t -> if x = var then !value else ref_lookup t x
 
 let rec lookup env x = 
   match env with
@@ -38,7 +38,7 @@ let ref_extend_tmp env x = (x, ref (Int 0))::env
 let rec ref_update env x v =
   match env with
   | [] -> raise (DeclareError ("Unbound variable " ^ x))
-  | (var, value)::t -> if x = var then (value := v) else ref-update t x v
+  | (var, value)::t -> if x = var then (value := v) else ref_update t x v
         
 (* Removes the most recent variable,value binding from the environment *)
 let rec remove env x = match env with
@@ -49,11 +49,164 @@ let rec remove env x = match env with
 
 (* Evaluates MicroCaml expression [e] in environment [env],
    returning a value, or throwing an exception on error *)
-let rec eval_expr env e = failwith "unimplemented"
+
+
+let not_helper expr = 
+  match expr with
+    |Bool woop -> Bool (not woop)
+    |_ -> raise (TypeError "haha")
+  
+
+let add_binop expr1 expr2 = 
+  match expr1, expr2 with
+    |Int a, Int b -> Int (a + b)
+    |_, _ -> raise (TypeError "haha")
+let sub_binop expr1 expr2 = 
+  match expr1, expr2 with
+    |Int a, Int b -> Int (a - b)
+    |_, _ -> raise (TypeError "haha")
+    
+let mult_binop expr1 expr2 = 
+  match expr1, expr2 with
+    |Int a, Int b -> Int (a * b)
+    |_, _ -> raise (TypeError "haha")
+
+let div_binop expr1 expr2 = 
+  match expr1, expr2 with
+    |Int a, Int b -> 
+      if b <> 0 then Int (a / b)
+      else raise (DivByZeroError)
+    |_, _ -> raise (TypeError "haha")
+
+let greater_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |Int a, Int b -> Bool (a > b)
+          |_, _ -> raise (TypeError "haha")
+
+let less_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |Int a, Int b -> Bool (a < b)
+          |_, _ -> raise (TypeError "haha")
+
+let greater_equal_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |Int a, Int b -> Bool (a >= b)
+          |_, _ -> raise (TypeError "haha")
+
+let less_equal_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |Int a, Int b -> Bool (a <= b)
+          |_, _ -> raise (TypeError "haha")
+
+let concat_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |String a, String b -> String (a ^ b)
+          |_, _ -> raise (TypeError "haha")
+
+let equal_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |Int a, Int b -> Bool(a = b)
+          |String a, String b -> Bool(a = b)
+          |Bool a, Bool b -> Bool(a = b)
+          |_, _ -> raise (TypeError "haha")
+
+let not_equal_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |Int a, Int b -> Bool(a <> b)
+          |String a, String b -> Bool(a <> b)
+          |Bool a, Bool b -> Bool(a <> b)
+          |_, _ -> raise (TypeError "haha")
+
+let or_binop expr1 expr2 = 
+  match expr1, expr2 with
+          |Bool a, Bool b -> Bool(a || b)
+          |_, _ -> raise (TypeError "haha")
+
+let and_binop expr1 expr2 = 
+  match expr1, expr2 with
+  |Bool a, Bool b -> Bool(a && b)
+  |_, _ -> raise (TypeError "haha")
+
+let binop_helper op expr1 expr2 = 
+    match op with
+      |Add -> add_binop expr1 expr2
+      |Sub -> sub_binop expr1 expr2
+      |Mult -> mult_binop expr1 expr2
+      |Div -> div_binop expr1 expr2
+      |Greater -> greater_binop expr1 expr2
+      |Less -> less_binop expr1 expr2
+      |GreaterEqual -> greater_equal_binop expr1 expr2
+      |LessEqual ->less_equal_binop expr1 expr2
+      |Concat -> concat_binop expr1 expr2
+      |Equal -> equal_binop expr1 expr2
+      |NotEqual -> not_equal_binop expr1 expr2
+      |Or -> or_binop expr1 expr2
+      |And -> and_binop expr1 expr2
+        
+
+  
+let rec eval_expr env e = match e with
+  |Value v -> v
+  |ID id -> ref_lookup env id
+  |Fun (var, expr) -> Closure (env, var, expr)
+  |Not expr -> 
+    let evaluated = eval_expr env expr in 
+    not_helper evaluated
+  |Binop (op, expr1, expr2) ->
+    let expr1 = eval_expr env expr1 in
+    let expr2 = eval_expr env expr2 in
+    binop_helper op expr1 expr2
+    
+  |If (exprBool, expr1, expr2) ->
+    let exprBool = eval_expr env exprBool in
+    eval_if env exprBool expr1 expr2
+
+  |FunctionCall (expr1, expr2) -> 
+    eval_function_call env expr1 expr2
+    
+
+  |Let (var, boool, expr1, expr2) ->
+    if boool then
+      let env = ref_extend_tmp env var in
+      let value = eval_expr env expr1 in
+      let _ = ref_update env var value in
+      eval_expr env expr2
+    else
+      let value = eval_expr env expr1 in 
+      eval_expr (ref_extend env var value) expr2
+
+and 
+eval_if env exprBool expr1 expr2 = 
+  match exprBool with
+    |Bool result -> 
+      if result = true then
+        eval_expr env expr1
+      else eval_expr env expr2
+    |_ -> raise (TypeError "haha")
+
+and 
+eval_function_call env expr1 expr2 = 
+  let expr1 = eval_expr env expr1 in 
+  let expr2 = eval_expr env expr2 in
+  match expr1, expr2 with 
+    |Closure (a, b, expr3), value -> 
+      let extended = ref_extend a b value in
+      eval_expr extended expr3
+    |_ -> raise (TypeError "haha")
+  
+
 
 (* Part 2: Evaluating mutop directive *)
 
 (* Evaluates MicroCaml mutop directive [m] in environment [env],
    returning a possibly updated environment paired with
    a value option; throws an exception on error *)
-let eval_mutop env m = failwith "unimplemented"
+let eval_mutop env m = match m with
+  |Def (var, expr)-> 
+    let temp = ref_extend_tmp env var in 
+    let hmm = eval_expr temp expr in
+    let _ = ref_update temp var hmm in
+    (temp, Some (hmm))
+  |Expr (expr) -> (env, Some (eval_expr env expr))
+  |NoOp -> (env, None)
+
--- _build/default/src/parser.ml
+++ _build/default/src/parser.ml
@@ -32,8 +32,197 @@ let rec lookahead_many (toks: token list) (n: int) =
   | _::t, n when n > 0 -> lookahead_many t (n-1)
   | _ -> None
 
-(* PASTE YOUR PARSERS FROM P4A HERE *)
+(* Part 2: Parsing expressions *)
 
-let rec parse_expr toks = failwith "unimplemented"
+let rec parse_expr toks = 
+  match lookahead toks with
+  | Some Tok_Let -> parse_let toks
+  | Some Tok_If -> parse_if toks
+  | Some Tok_Fun -> parse_fun toks
+  | _ -> parse_or toks
+  
+  and
+  parse_let toks = 
+    let token_let = match_token toks Tok_Let in
+    let exists_rec = (lookahead token_let = Some Tok_Rec) in
+    let token_rec = if exists_rec then match_token token_let Tok_Rec else token_let in
+    match lookahead token_rec with
+      | Some Tok_ID id -> 
+        let token_id = match_token token_rec (Tok_ID id) in
+        let token_equals = match_token token_id Tok_Equal in
+        let (token_list, expr1) = parse_expr token_equals in
+        let token_in = match_token token_list Tok_In in 
+        let (token_list, expr2) = parse_expr token_in in 
+        (token_list, Let (id, exists_rec, expr1, expr2))
+      |_ -> raise (InvalidInputException "haha")
 
-let rec parse_mutop toks = failwith "unimplemented"
+  and
+  parse_if toks = 
+    let token_if = match_token toks Tok_If in
+    let (token_list, expr1) = parse_expr token_if in
+    let token_then = match_token token_list Tok_Then in 
+    let (token_list, expr2) = parse_expr token_then in
+    let token_else = match_token token_list Tok_Else in 
+    let (token_list, expr3) = parse_expr token_else in 
+    (token_list, If (expr1, expr2, expr3))
+  
+  and
+  parse_fun toks =
+    let token_fun = match_token toks Tok_Fun in 
+    match lookahead token_fun with
+    | Some Tok_ID id -> 
+      let token_id = match_token token_fun (Tok_ID id) in
+      let token_arrow = match_token token_id Tok_Arrow in 
+      let (token_list, expr) = parse_expr token_arrow in
+      (token_list, Fun (id, expr))
+    |_ -> raise (InvalidInputException "haha")
+
+  and
+  parse_or toks = 
+      let (token_list, expr1) = parse_and toks in 
+      if lookahead token_list = Some Tok_Or then
+        let token_or = match_token token_list Tok_Or in 
+        let (token_list, expr2) = parse_or token_or in 
+        (token_list, Binop (Or, expr1, expr2))
+      else
+        (token_list, expr1)
+  
+  and
+  parse_and toks = 
+      let (token_list, expr1) = parse_equality toks in 
+      if lookahead token_list = Some Tok_And then
+        let token_and = match_token token_list Tok_And in 
+        let (token_list, expr2) = parse_and token_and in 
+        (token_list, Binop (And, expr1, expr2))
+      else
+        (token_list, expr1)
+
+  and
+  parse_equality toks = 
+      let (token_list, expr1) = parse_relational toks in
+      if lookahead token_list = Some Tok_Equal then 
+        let token_equals = match_token token_list Tok_Equal in
+        let (token_list, expr2) = parse_equality token_equals in 
+        (token_list, Binop (Equal, expr1, expr2))
+      else if lookahead token_list = Some Tok_NotEqual then
+        let token_equals = match_token token_list Tok_NotEqual in
+        let (token_list, expr2) = parse_equality token_equals in 
+        (token_list, Binop (NotEqual, expr1, expr2))
+      else
+        (token_list, expr1)
+  and
+  parse_relational toks = 
+    let (token_list, expr1) = parse_additive toks in
+    if lookahead token_list = Some Tok_Greater then 
+      let token_equals = match_token token_list Tok_Greater in
+      let (token_list, expr2) = parse_relational token_equals in 
+      (token_list, Binop (Greater, expr1, expr2))
+    else if lookahead token_list = Some Tok_Less then 
+      let token_equals = match_token token_list Tok_Less in
+      let (token_list, expr2) = parse_relational token_equals in 
+      (token_list, Binop (Less, expr1, expr2))
+    else if lookahead token_list = Some Tok_GreaterEqual then 
+      let token_equals = match_token token_list Tok_GreaterEqual in
+      let (token_list, expr2) = parse_relational token_equals in 
+      (token_list, Binop (GreaterEqual, expr1, expr2))
+    else if lookahead token_list = Some Tok_LessEqual then
+      let token_equals = match_token token_list Tok_LessEqual in
+      let (token_list, expr2) = parse_relational token_equals in 
+      (token_list, Binop (LessEqual, expr1, expr2))
+    else
+      (token_list, expr1)
+
+
+  and
+  parse_additive toks = 
+      let (token_list, expr1) = parse_mult toks in
+      if lookahead token_list = Some Tok_Add then 
+        let token_add = match_token token_list Tok_Add in 
+        let (token_list, expr2) = parse_additive token_add in 
+        (token_list, Binop (Add, expr1, expr2))
+      else if lookahead token_list = Some Tok_Sub then 
+        let token_add = match_token token_list Tok_Sub in 
+        let (token_list, expr2) = parse_additive token_add in 
+        (token_list, Binop (Sub, expr1, expr2))
+      else 
+        (token_list, expr1)
+
+  and
+  parse_mult toks = 
+      let (token_list, expr1) = parse_concat toks in 
+      if lookahead token_list = Some Tok_Mult then 
+        let token_mult = match_token token_list Tok_Mult in 
+        let (token_list, expr2) = parse_mult token_mult in 
+        (token_list, Binop (Mult, expr1, expr2))
+      else if lookahead token_list = Some Tok_Div then 
+        let token_mult = match_token token_list Tok_Div in 
+        let (token_list, expr2) = parse_mult token_mult in 
+        (token_list, Binop (Div, expr1, expr2))
+      else
+        (token_list, expr1)
+
+  and
+  parse_concat toks = 
+      let (token_list, expr1) = parse_unary toks in 
+      if lookahead token_list = Some Tok_Concat then 
+        let token_concat = match_token token_list Tok_Concat in 
+        let (token_list, expr2) = parse_concat token_concat in
+        (token_list, Binop (Concat, expr1, expr2))
+      else
+        (token_list, expr1)
+  
+  and
+  parse_unary toks = match lookahead toks with
+      |Some Tok_Not ->
+        let token_not = match_token toks Tok_Not in 
+        let (token_list, expr) = parse_unary token_not in 
+        (token_list, Not (expr))
+      |_ -> 
+        parse_func toks
+  
+  and
+  parse_func toks = 
+      let (token_list, expr1) = parse_primary toks in match lookahead token_list with
+      Some Tok_Int _ | Some Tok_Bool _ | Some Tok_String _ | Some Tok_ID _ | Some Tok_LParen -> 
+          let (token_list, expr2) = parse_primary token_list in 
+          (token_list, FunctionCall (expr1, expr2))
+      |_ ->
+        (token_list, expr1)
+
+
+
+  and
+  parse_primary toks = match lookahead toks with
+      |Some Tok_Int id -> (match_token toks (Tok_Int id), Value (Int (id)))
+      |Some Tok_Bool id -> (match_token toks (Tok_Bool id), Value (Bool (id)))
+      |Some Tok_String id -> (match_token toks (Tok_String id), Value (String (id)))
+      |Some Tok_ID id -> (match_token toks (Tok_ID id), ID (id))
+      |Some Tok_LParen-> 
+        let l_paren = match_token toks Tok_LParen in 
+        let (token_list, expr) = parse_expr l_paren in 
+        let r_paren = match_token token_list Tok_RParen in 
+        (r_paren, expr)
+      |_ -> raise (InvalidInputException "haha")
+
+
+
+
+
+(* Part 3: Parsing mutop *)
+
+let rec parse_mutop toks = match lookahead toks with
+    |Some Tok_DoubleSemi -> (match_token toks Tok_DoubleSemi, NoOp)
+    |Some Tok_Def -> parse_def toks
+    |_ -> let (token_list, expr1) = parse_expr toks in 
+    (match_token token_list Tok_DoubleSemi, Expr (expr1))
+
+  and
+  parse_def toks = 
+      let token_def = match_token toks Tok_Def in 
+      match lookahead token_def with 
+      |Some Tok_ID id -> 
+        let token_id = match_token token_def (Tok_ID id) in
+        let kill_equalsign = match_token token_id Tok_Equal in 
+        let (token_list, expr1) = parse_expr kill_equalsign in 
+        (match_token token_list Tok_DoubleSemi, Def (id, expr1))
+      |_ -> raise (InvalidInputException "haha")
